2e3b0277e14112a0d64ecdfc8ea2d6e70dc7a677
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-14 11:05:42 +0700

    Percobaan 1 Array 2

---
+++ Bioskop15.java
@@ -0,0 +1,20 @@
+public class Bioskop15 {  
+    public static void main(String[] args) {
+
+        String[][] penonton = new String[4][2];
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+
+        System.out.println("Daftar Penonton Bioskop:");
+        System.out.println(penonton[0][0] + "\t" + penonton[0][1]);
+        System.out.println(penonton[1][0] + "\t" + penonton[1][1]);
+        System.out.println(penonton[2][0] + "\t" + penonton[2][1]);
+        System.out.println(penonton[3][0] + "\t" + penonton[3][1]);
+    }
+}
