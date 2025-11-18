703e6760ea44b1e90f5c065bd5e848656280463c
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-18 14:28:02 +0700

    Modifikasi Percobaan 2 No.2

--- BioskopWithScanner15.java
+++ BioskopWithScanner15.java
@@ -3,43 +3,54 @@ import java.util.Scanner;
 public class BioskopWithScanner15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String nama, next;
-        int baris, kolom;
-
+        
         String[][] penonton = new String[4][2];
-
+        
+        int pilihan;
+        
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
+            System.out.println("\n=== MENU BIOSKOP ===");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1-3): ");
+            pilihan = sc.nextInt();
             sc.nextLine();
-
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
-            sc.nextLine();
-
-            penonton[baris - 1][kolom - 1] = nama;
-
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
-                break;
-            }
-        }
-
-        System.out.println("\nData Penonton:");
-        for (int i = 0; i < penonton.length; i++) {
-            for (int j = 0; j < penonton[i].length; j++) {
-                if (penonton[i][j] != null) {
-                    System.out.println("Baris " + (i + 1) + " Kolom " + (j + 1) + ": " + penonton[i][j]);
+            
+            if (pilihan == 1) {
+                System.out.print("Masukkan nama: ");
+                String nama = sc.nextLine();
+                
+                System.out.print("Masukkan baris: ");
+                int baris = sc.nextInt();
+                sc.nextLine();
+                
+                System.out.print("Masukkan kolom: ");
+                int kolom = sc.nextInt();
+                sc.nextLine();
+                
+                penonton[baris-1][kolom-1] = nama;
+                System.out.println("Data penonton berhasil disimpan!");
+                
+            } else if (pilihan == 2) {
+                System.out.println("\n=== DAFTAR PENONTON ===");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] != null) {
+                            System.out.println("Baris " + (i+1) + " Kolom " + (j+1) + ": " + penonton[i][j]);
+                        }
+                    }
                 }
+                
+            } else if (pilihan == 3) {
+                System.out.println("Terima kasih! Program selesai.");
+                break;
+                
+            } else {
+                System.out.println("Pilihan tidak valid! Silakan pilih 1-3.");
             }
         }
-
+        
         sc.close();
     }
 }
