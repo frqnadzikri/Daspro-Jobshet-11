38218be6f7e55c520f3ba4921bcaeeab0edbea48
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-19 22:12:43 +0700

    Modifikasi Percobaan 3 No.1

--- Numbers15.java
+++ Numbers15.java
@@ -1,16 +1,14 @@
+import java.util.Arrays;
+
 public class Numbers15 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
-    
         myNumbers[0] = new int[5];
-        
         myNumbers[1] = new int[3];
-        
         myNumbers[2] = new int[1];
         
-        System.out.println("Jumlah baris: " + myNumbers.length);
-        System.out.println("Jumlah kolom baris ke-1: " + myNumbers[0].length);
-        System.out.println("Jumlah kolom baris ke-2: " + myNumbers[1].length);
-        System.out.println("Jumlah kolom baris ke-3: " + myNumbers[2].length);
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }
