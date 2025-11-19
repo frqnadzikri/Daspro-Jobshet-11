6981222e566a9bd9fff7aa8e4dca9d0614fbf0af
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-19 22:44:16 +0700

    Modifikasi Percobaan 4 No.8

--- SIAKAD15.java
+++ SIAKAD15.java
@@ -5,19 +5,33 @@ public class SIAKAD15 {
         Scanner sc = new Scanner(System.in);
         int[][] nilai = new int[4][3];
 
-        for (int i = 0; i < nilai.length; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
-            for (int j = 0; j < nilai[i].length; j++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print("Nilai mata kuliah " + (j+1) + ": ");
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3 + "\n");
+            System.out.println("Nilai rata-rata mahasiswa " + (i+1) + ": " + totalPerSiswa / 3 + "\n");
+        }
+
+        System.out.println("\n==============================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+
+        for (int j = 0; j < 3; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < 4; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
         }
 
         sc.close();
     }
 }
+
