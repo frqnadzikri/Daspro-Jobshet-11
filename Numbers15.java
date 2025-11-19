1cdc075532f03548113850bb75fd93bc587ac2ec
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-19 22:19:07 +0700

    Modifikasi Percobaan 3 No.4

--- Numbers15.java
+++ Numbers15.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Numbers15 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
@@ -8,7 +6,7 @@ public class Numbers15 {
         myNumbers[2] = new int[1];
         
         for (int i = 0; i < myNumbers.length; i++) {
-            System.out.println(Arrays.toString(myNumbers[i]));
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
         }
     }
 }
